c796df8383f866652cf7d453f4a808fc05c4f4fc
Chung Nguyen <zrmedia9@gmail.com>
2021-10-17 21:17:13 +0700

    Project init

---
+++ lib/widgets/new_transaction.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class NewTransaction extends StatelessWidget {
+  final Function addTx;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  NewTransaction(this.addTx);
+
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+
+    addTx(titleController.text, double.parse(amountController.text));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'Title'),
+              // onChanged: (val) => titleInput = val
+              controller: titleController,
+              onSubmitted: (_) => submitData(),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Amount'),
+              // onChanged: (val) => amountInput = val
+              controller: amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => submitData(),
+            ),
+            TextButton(
+              onPressed: submitData,
+              child: Text('Add Transaction'),
+              style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all(Colors.purple)),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
